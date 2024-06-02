fe29d8edd630386fd6c83728e297f93c41286532
kajal singh <133157917+kajalsingh8@users.noreply.github.com>
2024-06-02 16:56:51 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find Indexes of a subarray with given sum/find-indexes-of-a-subarray-with-given-sum.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main{
+	static BufferedReader br;
+    static PrintWriter ot;
+    public static void main(String[] args) throws IOException{
+        
+        br = new BufferedReader(new InputStreamReader(System.in));
+        ot = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(br.readLine());
+
+        while(t-->0){
+            
+            String s[] = br.readLine().trim().split(" ");
+            
+            int n = Integer.parseInt(s[0]);
+            int k = Integer.parseInt(s[1]);
+            int a[] = new int[n];
+            s = br.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+                a[i] = Integer.parseInt(s[i]);
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.subarraySum(a, n, k);
+            for(int ii = 0;ii<res.size();ii++)
+                ot.print(res.get(ii) + " ");
+            ot.println();
+        }
+        ot.close();
+    }
+
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+  static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
+    {
+        // Your code here
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        int low = 0;
+        int high = 0;
+        int currSum = 0;
+        while(high < n){
+            currSum += arr[high];
+            while(currSum > s && low<high){
+                currSum -= arr[low];
+                low++;
+            }
+            if(currSum == s){
+                res.add(low+1);
+                res.add(high+1);
+                return res;
+            }
+            high++;
+        }
+        res.add(-1);
+        return res;
+    }
+
+
+}
