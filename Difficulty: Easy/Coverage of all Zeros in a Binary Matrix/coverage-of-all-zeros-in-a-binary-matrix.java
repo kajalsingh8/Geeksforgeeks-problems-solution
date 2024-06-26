f12aaa029fe824d15af89a250e9e538c5ddb8e06
kajal singh <133157917+kajalsingh8@users.noreply.github.com>
2024-06-26 16:21:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Coverage of all Zeros in a Binary Matrix/coverage-of-all-zeros-in-a-binary-matrix.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int m = Integer.parseInt(s[1]);
+            int[][] matrix = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                String[] S = br.readLine().split(" ");
+                for (int j = 0; j < m; j++) {
+                    matrix[i][j] = Integer.parseInt(S[j]);
+                }
+            }
+            Solution ob = new Solution();
+            int ans = ob.findCoverage(matrix);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int findCoverage(int[][] matrix) {
+        int n=matrix.length;
+        int count=0;
+        int m=matrix[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    try{
+                        if(matrix[i-1][j]==1)
+                          count++;
+                    }
+                    catch(Exception e){}
+                    try{
+                        if(matrix[i][j-1]==1)
+                          count++;
+                    }
+                    catch(Exception e){}
+                    try{
+                        if(matrix[i+1][j]==1)
+                          count++;
+                    }
+                    catch(Exception e){}
+                    try{
+                        if(matrix[i][j+1]==1)
+                          count++;
+                    }
+                    catch(Exception e){}
+                }
+            }
+        }
+        return count;
+    }
+}
+
