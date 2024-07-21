70b02a97feab04d697201582b302db9ebe2ab6d8
kajal singh <133157917+kajalsingh8@users.noreply.github.com>
2024-07-21 18:40:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -13,7 +13,39 @@ class Solution
     public static void sort012(int a[], int n)
     {
         // code here 
-        Arrays.sort(a);
+        //Arrays.sort(a);
+        int low=0;
+        int mid=0;
+        int high=n-1;
+    
+    
+         while(mid<=high){
+             
+             if(a[mid]==0){
+                 swap(a,low,mid);
+                 low++;
+                 mid++;
+                 
+             }
+             else if(a[mid]==1){
+                 mid++;
+             }
+             else{
+                 swap(a,mid,high);
+                 high--;
+             }
+         }
+    }
+    
+    private static void swap(int a[],int x,int y){
+         
+   
+        
+        int temp=a[x];
+        a[x]=a[y];
+        a[y]=temp;
+        
+      
     }
 }
 
