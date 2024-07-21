6aa615ce4eea91d9c507330c6de4510e539ec31b
kajal singh <133157917+kajalsingh8@users.noreply.github.com>
2024-07-21 21:15:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Duplicates/array-duplicates.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            int[] arr = IntArray.input(br, n);
+
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.duplicates(arr);
+
+            IntArray.print(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static ArrayList<Integer> duplicates(int[] arr) {
+        // code here
+        
+        ArrayList<Integer>ans=new ArrayList<>();
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            int x=arr[i];
+            for(int j=0;j<arr.length;j++){
+                if(arr[j]==x && i!=j){
+                    count++;
+                }
+            }
+            if(count>1){
+                ans.add(x);
+            }
+        }
+        
+        if(ans.size()==0){
+             ans.add(-1);
+             return ans;
+        }
+        
+        return ans;
+        
+    }
+}
