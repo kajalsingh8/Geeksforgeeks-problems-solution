bbe63e93039fb86cfa38e4e856f051c5c9f4deaf
kajal singh <133157917+kajalsingh8@users.noreply.github.com>
2024-07-29 20:24:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Introduction to Linked List/introduction-to-linked-list.java
+++ Difficulty: Easy/Introduction to Linked List/introduction-to-linked-list.java
@@ -54,10 +54,10 @@ class Solution {
         if(arr.length==0)
             return null;
             Node head = new Node(arr[0]);
-            Node t = head;
+            Node temp = head;
             for(int i =1;i<arr.length;i++){
-                t.next = new Node(arr[i]);
-                t = t.next;
+                temp.next = new Node(arr[i]);
+                temp = temp.next;
             }
         
         return head;
