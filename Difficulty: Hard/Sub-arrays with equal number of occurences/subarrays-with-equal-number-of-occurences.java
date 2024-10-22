ea864b2e63e60a33579edef2600e3b020f6e2c14
kajal singh <133157917+kajalsingh8@users.noreply.github.com>
2024-10-22 19:30:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Sub-arrays with equal number of occurences/subarrays-with-equal-number-of-occurences.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.util.HashMap;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+
+    static int sameOccurrence(int arr[], int x, int y) {
+        // write code here
+        Map<Integer, Integer> diffMap = new HashMap<>();
+        diffMap.put(0,1);
+        int diff = 0;
+        int count = 0;
+        for(int num : arr){
+            if(num == x){
+                diff++;
+         }else if ( num == y){
+             diff--;
+         }
+         if(diffMap.containsKey(diff)){
+             count += diffMap.get(diff);
+         }
+         diffMap.put(diff, diffMap.getOrDefault(diff,0) + 1);
+        }
+        
+        return count;
+    }
+}
+
+
+
+
+//{ Driver Code Starts.
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            int x = Integer.parseInt(br.readLine());
+            int y = Integer.parseInt(br.readLine());
+            // Create Solution object and find closest sum
+            Solution ob = new Solution();
+            int ans = ob.sameOccurrence(arr, x, y);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
