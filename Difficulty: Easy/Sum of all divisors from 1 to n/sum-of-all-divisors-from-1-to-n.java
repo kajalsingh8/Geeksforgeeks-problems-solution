ac300cd414c61b51bb029c05b30896726e596be8
kajal singh <133157917+kajalsingh8@users.noreply.github.com>
2024-11-24 20:58:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sum of all divisors from 1 to n/sum-of-all-divisors-from-1-to-n.java
+++ Difficulty: Easy/Sum of all divisors from 1 to n/sum-of-all-divisors-from-1-to-n.java
@@ -1,20 +1,17 @@
 //{ Driver Code Starts
-//Initial Template for Java
 import java.io.*;
 import java.util.*;
 
-class GFG
-{
-    public static void main(String args[])throws IOException
-    {
+class Main {
+    public static void main(String args[]) throws IOException {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        while(t-- > 0)
-        {
-            int N=sc.nextInt();
+        while (t-- > 0) {
+            int N = sc.nextInt();
             Solution ob = new Solution();
-            long ans  = ob.sumOfDivisors(N);
+            int ans = ob.sumOfDivisors(N);
             System.out.println(ans);
+            System.out.println("~");
         }
     }
 }
@@ -22,13 +19,11 @@ class GFG
 // } Driver Code Ends
 
 
-//User function Template for Java
-
-//User function Template for Java
+// User function Template for Java
 class Solution{
-    static long sumOfDivisors(int N){
+    static int sumOfDivisors(int N){
         // code here
-        long ans = 0;
+        int ans = 0;
         for(int i =1;i<=N;i++){
             ans += (N/i)*i; 
         }
