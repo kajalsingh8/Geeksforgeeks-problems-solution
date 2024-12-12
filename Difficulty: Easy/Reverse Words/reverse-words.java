af7b7a59b6909db15a4259569e7eca5a21ebfa7a
kajal singh <133157917+kajalsingh8@users.noreply.github.com>
2024-12-12 14:11:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse Words/reverse-words.java
+++ Difficulty: Easy/Reverse Words/reverse-words.java
@@ -1,40 +1,51 @@
 //{ Driver Code Starts
+// Initial Template for Java
+
 import java.io.*;
-import java.lang.*;
 import java.util.*;
+import java.util.Scanner;
 
-class GFG {
+class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t > 0) {
-            String s = sc.next();
-            Solution obj = new Solution();
-            System.out.println(obj.reverseWords(s));
-            t--;
+
+        int t = sc.nextInt(); // Read the number of test cases
+        sc.nextLine();        // Consume the newline character after reading the integer
+
+        while (t-- > 0) {
+            String s = sc.nextLine(); // Read the input string
+
+            // Remove surrounding quotes from the string
+            String str = s.substring(1, s.length() - 1);
+
+            Solution obj = new Solution(); // Create an object of the Solution class
+            String ans = obj.reverseWords(str); // Reverse the words in the string
+
+            // Print the result with surrounding quotes
+            System.out.println("\"" + ans + "\"");
         }
+        sc.close();
     }
 }
 
 // } Driver Code Ends
 
 
-
 class Solution {
-    // Function to reverse words in a given string.
-    String reverseWords(String str) {
-        // code here
-        String reverse = "";
-        String temp = "";
-        for(int i =0;i < str.length();i++){
-            if(str.charAt(i) != '.'){
-                temp = temp + str.charAt(i);
-            }else{
-                reverse = '.'+temp + reverse;
-                temp ="";
-            }
+    // Function to reverse words in a given string
+    public String reverseWords(String s) {
+        String[] str = s.split("\\s+");
+        Stack st = new Stack();
+        for(String strs: str){
+            st.push(strs);
         }
-        return temp + reverse;
+        s="";
+        while(!(st.isEmpty())){
+            s = s+(st.pop());
+            s=s+" ";
+        }
+        s= s.trim();
+        return s;
     }
 }
+
