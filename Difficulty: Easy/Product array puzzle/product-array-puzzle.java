c4c6e99ade34fd600cc0688943638cac1b706327
kajal singh <133157917+kajalsingh8@users.noreply.github.com>
2025-01-17 11:43:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Product array puzzle/product-array-puzzle.java
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.java
@@ -6,22 +6,24 @@ import java.util.*;
 
 class GFG {
     public static void main(String args[]) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
         while (t > 0) {
-            int n = sc.nextInt();
-            int[] array = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = inputLine.length;
+            int arr[] = new int[n];
             for (int i = 0; i < n; i++) {
-                array[i] = sc.nextInt();
+                arr[i] = Integer.parseInt(inputLine[i]);
             }
             Solution ob = new Solution();
-            long[] ans = new long[n];
-            ans = ob.productExceptSelf(array);
+            int[] ans = new int[n];
+            ans = ob.productExceptSelf(arr);
 
             for (int i = 0; i < n; i++) {
                 System.out.print(ans[i] + " ");
             }
             System.out.println();
+            System.out.println("~");
             t--;
         }
     }
@@ -31,28 +33,26 @@ class GFG {
 
 
 // User function Template for Java
-
 class Solution {
-    public static long[] productExceptSelf(int nums[]) {
-    long temp = 1;
-        long c = 0; 
-        int index = -1;
-        for(int i = 0 ;i<nums.length;i++){
-            if(nums[i]!=0) temp = temp*nums[i];
-            else{
-                c++;
-                index = i;
+    public static int[] productExceptSelf(int arr[]) {
+        // code here
+        int pro = 1, zero = 0;
+        for(int i = 0; i<arr.length;i++){
+            if(arr[i] !=0){
+                pro*=arr[i];
+            }else{
+                zero++;
             }
         }
-        long ans [] = new long[nums.length];
-        if(c>1) return ans;
-        if(c==1){
-            ans[index] = temp;
-            return ans;
+        for(int i = 0;i<arr.length;i++){
+            if(zero>1){
+                arr[i] =0;
+            }else if(zero == 1){
+                arr[i]= arr[i]!=0?0:pro;
+            }else{
+                arr[i]=pro/arr[i];
+            }
         }
-        for(int i  =0 ; i<nums.length;i++){
-            ans[i] = temp/nums[i];
-        }
-        return ans;
+        return arr;
     }
 }
