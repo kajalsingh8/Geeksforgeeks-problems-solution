8d4f1666ee83c65abea4ae439520a871a8ff1607
kajal singh <133157917+kajalsingh8@users.noreply.github.com>
2025-04-21 21:44:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.java
+++ Difficulty: Easy/Missing in Array/missing-in-array.java
@@ -1,45 +1,34 @@
 //{ Driver Code Starts
-// Initial Template for Java
-
 import java.io.*;
 import java.util.*;
 
-class GFG {
-
-    public static void main(String[] args) throws IOException {
+class Geeks {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine().trim());
-        while (t-- > 0) {
-            int n = Integer.parseInt(br.readLine().trim());
-            String[] str = br.readLine().trim().split(" ");
-            int[] arr = new int[n - 1];
-            for (int i = 0; i < n - 1; i++) {
+        int t = Integer.parseInt(br.readLine());
+        for (int g = 0; g < t; g++) {
+            String[] str = (br.readLine()).trim().split(" ");
+            int arr[] = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
                 arr[i] = Integer.parseInt(str[i]);
             }
-            Solution sln = new Solution();
-            System.out.println(sln.missingNumber(n, arr));
+            System.out.println(new Solution().missingNum(arr));
+            System.out.println("~");
         }
     }
 }
 // } Driver Code Ends
 
 
-// User function Template for Java
 class Solution {
-
-    // Note that the size of the array is n-1
-    int missingNumber(int n, int arr[]) {
-      
-        int originalSum = n *( n+1)/2;
-        int  takensum = 0;
-        for(int i =0;i <arr.length;i++){
-            takensum += arr[i];
-
-        }
-    
-        return originalSum - takensum;    
-         
-        // Your Code Here
+    int missingNum(int arr[]) {
+        // code here
+       long sum=0,max=0;
+       for(long i : arr){
+           if(i>max) max=i;
+           sum+=i;
+       }
+       if(sum==max*(max+1)/2) return (int)max+1;
+       return (int)(max*(max+1)/2 - sum);
     }
 }
